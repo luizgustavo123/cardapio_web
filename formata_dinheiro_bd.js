f7294e4cae6bf9b733cb623c60936c8dbd0cf6a8
Luiz Gustavo Vieira da Silva <33435097+luizgustavo123@users.noreply.github.com>
2024-07-03 19:54:15 -0300

    Add files via upload

---
+++ formata_dinheiro_bd.js
@@ -0,0 +1,13 @@
+﻿
+function formata_dinheiro(valor) {
+    // VERIFICA SE O VALOR DO PRODUTO É MAIOR QUE 1000 PARA FORMATAR O VALOR CORRETAMENTE
+    if (parseInt(valor.toString().replaceAll(",", "").replaceAll(".", "").length) > 5) {
+        valor_format = valor.replace(".", "");
+    } else {
+        valor_format = valor.replace(",", ".");
+    }
+
+    valor_format = valor_format.replace(",", ".");
+
+    return valor_format
+}
